b6346c205ed419fa8adac86485fd6366fc4bc355
David Espinoza <despinoza1027@gmail.com>
2017-07-15 20:03:32 -0500

    completed gigasecond challenge

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,3 +1,18 @@
 // Calculate the moment when someone has lived for 10^9 seconds.
 //
 // A gigasecond is 10^9 (1,000,000,000) seconds.
+
+var Gigasecond = function (time) {
+   this.time = time;
+}
+
+// 1000000000000 = (1e12)
+
+Gigasecond.prototype.date = function () {
+  let gsTime = this.time.getTime() + (1e12);
+  let newTime = new Date();
+  newTime.setTime(gsTime);
+  return newTime;
+};
+
+module.exports = Gigasecond;
